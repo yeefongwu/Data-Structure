b50c05a1434f2b71abb4d4ee709a3d3285249137
Wyatt-Ng <79526658+yeefongwu@users.noreply.github.com>
2021-07-01 10:05:49 +0800

    Add files via upload

---
+++ lab/lab13/cmake-build-debug/CMakeFiles/lab13.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab13.dir/check2.cpp.o"
+  "CMakeFiles/lab13.dir/check3.cpp.o"
+  "lab13"
+  "lab13.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
